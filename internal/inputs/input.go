fbd52872919e968bdfc2315e97863b272b39143c
louna <r.arswendo.r@gmail.com>
2024-08-30 20:25:46 +0700

    tested basic renderers

---
+++ internal/inputs/input.go
@@ -0,0 +1,17 @@
+package inputs
+
+import (
+	"fmt"
+
+	"github.com/veandco/go-sdl2/sdl"
+)
+
+func HandleInput(callback func()) {
+	for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+		switch event.(type) {
+		case *sdl.QuitEvent:
+			fmt.Println("Quit")
+			callback()
+		}
+	}
+}
--- main.go
+++ main.go
@@ -1,42 +1,62 @@
 package main
 
-import "github.com/veandco/go-sdl2/sdl"
+import (
+	"Lunarisnia/sdl-pong/internal/inputs"
+
+	"github.com/veandco/go-sdl2/sdl"
+)
+
+const (
+	screenWidth  = 640
+	screenHeight = 480
+)
+
+type App struct {
+	Window   *sdl.Window
+	Renderer *sdl.Renderer
+}
 
 func main() {
 	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
 		panic(err)
 	}
 	defer sdl.Quit()
+	sdl.Quit()
+	app := App{}
 
-	window, err := sdl.CreateWindow("test", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED, 800, 600, sdl.WINDOW_SHOWN)
+	var err error
+	app.Window, err = sdl.CreateWindow("testis", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED, screenWidth, screenHeight, sdl.WINDOW_SHOWN)
 	if err != nil {
 		panic(err)
 	}
-	defer window.Destroy()
+	defer app.Window.Destroy()
 
-	surface, err := window.GetSurface()
+	app.Renderer, err = sdl.CreateRenderer(app.Window, -1, sdl.RENDERER_ACCELERATED)
 	if err != nil {
 		panic(err)
 	}
-	surface.FillRect(nil, 0)
+	defer app.Renderer.Destroy()
 
-	rect := sdl.Rect{0, 0, 200, 200}
-	colour := sdl.Color{R: 255, G: 0, B: 255, A: 255} // purple
-	pixel := sdl.MapRGBA(surface.Format, colour.R, colour.G, colour.B, colour.A)
-	surface.FillRect(&rect, pixel)
-	window.UpdateSurface()
+	rect := sdl.Rect{X: -300, Y: 0, W: 100, H: 100}
 
 	running := true
 	for running {
-		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
-			switch event.(type) {
-			case *sdl.QuitEvent: // NOTE: Please use `*sdl.QuitEvent` for `v0.4.x` (current version).
-				println("Quit")
-				running = false
-				break
-			}
-		}
+		app.Renderer.SetDrawColor(0, 0, 0, 255)
+		app.Renderer.Clear()
 
+		app.Renderer.SetDrawColor(255, 255, 255, 255)
+		app.Renderer.DrawRect(&rect)
+
+		inputs.HandleInput(func() {
+			running = false
+		})
+
+		app.Renderer.Present()
 		sdl.Delay(33)
+
+		rect.X += 10
+		if rect.X == 740 {
+			rect.X = -300
+		}
 	}
 }
