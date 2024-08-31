648d3a515498b99a43064ea2d197bf4db270f660
louna <r.arswendo.r@gmail.com>
2024-08-31 18:57:57 +0700

    basic rendering setup

--- internal/graphics/draw.go
+++ internal/graphics/draw.go
@@ -1,12 +1,46 @@
 package graphics
 
-import "Lunarisnia/sdl-pong/internal/core"
+import (
+	"Lunarisnia/sdl-pong/internal/dsu"
+	"github.com/veandco/go-sdl2/img"
+	"github.com/veandco/go-sdl2/sdl"
+)
 
-func PrepareScene(app *core.App) {
-	app.Renderer.SetDrawColor(96, 128, 255, 255)
-	app.Renderer.Clear()
+func PrepareScene(r *sdl.Renderer) {
+	r.SetDrawColor(96, 128, 255, 255)
+	r.Clear()
 }
 
-func PresentScene(app *core.App) {
-	app.Renderer.Present()
+func PresentScene(r *sdl.Renderer) {
+	r.Present()
+}
+
+func LoadTexture(r *sdl.Renderer, filename string) (*sdl.Texture, error) {
+	sdl.LogMessage(sdl.LOG_CATEGORY_APPLICATION, sdl.LOG_PRIORITY_INFO, "Loading %s", filename)
+	texture, err := img.LoadTexture(r, filename)
+	if err != nil {
+		return nil, err
+	}
+
+	return texture, nil
+}
+
+func Blit(r *sdl.Renderer, texture *sdl.Texture, position dsu.Vector2i) error {
+	dest := sdl.Rect{
+		X: position.X,
+		Y: position.Y,
+	}
+	_, _, width, height, err := texture.Query()
+	if err != nil {
+		return err
+	}
+	dest.W = width
+	dest.H = height
+
+	err = r.Copy(texture, nil, &dest)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"Lunarisnia/sdl-pong/internal/core"
+	"Lunarisnia/sdl-pong/internal/dsu"
 	"Lunarisnia/sdl-pong/internal/graphics"
 	"Lunarisnia/sdl-pong/internal/inputs"
 	"github.com/veandco/go-sdl2/sdl"
@@ -14,16 +15,32 @@ func main() {
 	defer app.Window.Destroy()
 	defer app.Renderer.Destroy()
 
+	player, err := graphics.LoadTexture(app.Renderer, "assets/player.png")
+	if err != nil {
+		panic(err)
+	}
+
+	playerPosition := dsu.Vector2i{
+		X: 0,
+		Y: 0,
+	}
+
 	running := true
 	for running {
-		graphics.PrepareScene(app)
+		graphics.PrepareScene(app.Renderer)
+
+		graphics.Blit(app.Renderer, player, playerPosition)
 
 		inputs.HandleInput(func() {
 			running = false
 		})
 
-		graphics.PresentScene(app)
+		graphics.PresentScene(app.Renderer)
 
 		sdl.Delay(16)
+
+		playerPosition = playerPosition.Add(dsu.Vector2i{X: 1, Y: 1})
+		playerPosition.X %= 500
+		playerPosition.Y %= 500
 	}
 }
